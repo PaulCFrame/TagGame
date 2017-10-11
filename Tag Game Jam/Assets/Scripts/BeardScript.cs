a5e0cf1a273d175c047ab451412aa4eddc079bc0
PaulCFrame <1008164@student.sae.edu.au>
2017-10-11 19:18:44 +1000

    Da PUUUUUUUUUUUUSh

---
+++ Tag Game Jam/Assets/Scripts/BeardScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeardScript : MonoBehaviour {
+
+    public float maxScale;
+    public float minScale;
+    public float addition;
+    public GameObject parent;
+
+    bool beardExtendo;
+
+	// Use this for initialization
+	void Start ()
+    {
+        minScale = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            beardExtendo = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            beardExtendo = false;
+        }
+        
+       // transform.position = parent.transform.position;
+
+        if (beardExtendo)
+        {
+            if (transform.localScale.z < maxScale)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z + Time.deltaTime * addition);
+                transform.position = parent.transform.position + transform.localScale.z / 2 * -parent.transform.forward;
+            }
+            else
+            {
+                transform.position = parent.transform.position + transform.localScale.z / 2 * -parent.transform.forward;
+            }
+        }
+
+        if (!beardExtendo && transform.localScale.z > minScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x , transform.localScale.y, transform.localScale.z - Time.deltaTime * addition);
+            transform.position = parent.transform.position + transform.localScale.z / 2 * -parent.transform.forward;
+        }
+    }
+}

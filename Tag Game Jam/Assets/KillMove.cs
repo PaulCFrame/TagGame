0020cad694270b32102b27092e01910b32e4f4ff
SAE\tshaw3 <tristanjulianshaw@gmail.com>
2017-10-11 20:26:27 +1000

    no message

---
+++ Tag Game Jam/Assets/KillMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillMove : MonoBehaviour
+{
+    public GameObject paulWins;
+    public GameObject tristanWins;
+    public GameObject paul;
+    public GameObject tristan;
+    public bool gameOver;
+
+    public void Start()
+    {
+        gameOver = false;
+    }
+
+    public void Update()
+    {
+        if (gameOver == true)
+        {
+            if (Input.anyKey)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("PaulScene");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "paul")
+        {
+            //disable script that locks position
+            tristanWins.SetActive(true);
+            paul.GetComponent<PaulMove>().enabled = false;
+            gameOver = true;
+        }
+
+        if (other.gameObject.tag == "tristan")
+        {
+            //disable script that locks position
+            paulWins.SetActive(true);
+            paul.GetComponent<TristanMove>().enabled = false;
+            gameOver = true;
+        }
+    }
+}
